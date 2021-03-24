69af0e3a0a5f3f01eb91f78fba7bd43962462bf2
Seba <seba_msdstudio@hotmail.com>
2021-03-24 13:06:38 -0300

    Funciones

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 let nombre = 'SEBA';
-
+let edad = 27;
 console.log(`Su nombre es ${nombre}`);
+
+function mayorDeEdad(age,name) {
+    if (age >= 18) {
+        console.log(`${name} es mayor de Edad
+        Por lo tanto puede conducir`);
+    }else{
+        console.log(`lamentablemente ${name}
+        no es mayor de edad`);
+    }
+}
